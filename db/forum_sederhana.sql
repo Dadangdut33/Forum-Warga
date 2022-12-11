940ea3baf17372097ff09f2d2d54c2f7e99ed20b
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-12-12 00:32:58 +0700

    db query update

---
+++ db/forum_sederhana.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 11, 2022 at 05:29 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `forum_sederhana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(1000) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `userID` varchar(50) NOT NULL,
+  `postID` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_UserComment` (`userID`),
+  KEY `FK_CommentPost` (`postID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+DROP TABLE IF EXISTS `notification`;
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `details` varchar(500) NOT NULL,
+  `link` varchar(500) DEFAULT NULL,
+  `type` varchar(300) DEFAULT NULL,
+  `isRead` tinyint(1) DEFAULT '0',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `userID` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_UserNotif` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `notification`
+--
+
+INSERT INTO `notification` (`id`, `details`, `link`, `type`, `isRead`, `time`, `userID`) VALUES
+(1, 'Jelek lu', '#', 'Comment Deleted By Admin', 1, '2022-11-30 13:47:43', 'Password123'),
+(2, 'Jelek lagi lu', '#', 'Post Deleted By Admin', 1, '2022-11-30 13:47:49', 'Password123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `content` varchar(5000) DEFAULT NULL,
+  `pinned` int(1) NOT NULL DEFAULT '0',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `views` bigint(20) NOT NULL DEFAULT '0',
+  `userID` varchar(50) NOT NULL,
+  `topicID` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_UserPost` (`userID`),
+  KEY `FK_PostTopic` (`topicID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id`, `title`, `content`, `pinned`, `time`, `views`, `userID`, `topicID`) VALUES
+(2, 'test  ', 'dasd asd asd asd asdasdasdasdasdasdadasdasdasdadasdas ', 0, '2022-11-30 13:44:42', 18, 'Fauzan', 7),
+(4, '105 OR 1=1', '105 OR 1=1 dasd asd sadasdasdasdads', 1, '2022-11-30 13:56:17', 4, 'Password123', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topic`
+--
+
+DROP TABLE IF EXISTS `topic`;
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `topic`
+--
+
+INSERT INTO `topic` (`id`, `name`) VALUES
+(1, 'General'),
+(2, 'Games'),
+(3, 'Movies'),
+(4, 'Music'),
+(5, 'Science'),
+(6, 'Random Discussion');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `isAdmin` tinyint(1) DEFAULT '0',
+  `bio` varchar(500) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `email`, `isAdmin`, `bio`) VALUES
+('Password123', '$2y$10$lIIvIdJa9wy60cKvH/GoBeaSVcS4iV/x1ZeVWHsDAT3UHqv21ABQ2', 'myemail@gmail.com', 1, ''),
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `web_config`
+--
+
+DROP TABLE IF EXISTS `web_config`;
+CREATE TABLE IF NOT EXISTS `web_config` (
+  `id` int(11) NOT NULL DEFAULT '1',
+  `forum_name` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `web_config`
+--
+
+INSERT INTO `web_config` (`id`, `forum_name`) VALUES
+(1, 'Sukaforum');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `FK_CommentPost` FOREIGN KEY (`postID`) REFERENCES `post` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_UserComment` FOREIGN KEY (`userID`) REFERENCES `users` (`username`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `notification`
+--
+ALTER TABLE `notification`
+  ADD CONSTRAINT `FK_UserNotif` FOREIGN KEY (`userID`) REFERENCES `users` (`username`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `FK_PostTopic` FOREIGN KEY (`topicID`) REFERENCES `topic` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_UserPost` FOREIGN KEY (`userID`) REFERENCES `users` (`username`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
